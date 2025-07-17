2ab67334a749c4ddddf32d366c8a440ea0c25d3c
Alebilli98 <bullettiale9@gmail.com>
2025-07-17 13:41:41 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+const password = "TeamFinSight";
+const inserita = prompt("Inserisci la password:");
+if (inserita !== password) {
+  document.body.innerHTML = "<h1>Accesso negato</h1>";
+  throw new Error("Accesso bloccato");
+}
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
